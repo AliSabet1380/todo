312ffd69af2812292ea057ebdd3e7017ea74d616
AliSabet <ali8551sbt@gmail.com>
2023-12-18 10:59:24 +0330

    new update

--- src/app/auth/callback/route.jsx
+++ src/app/auth/callback/route.jsx
@@ -1,17 +1,19 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
   const cookieStore = cookies();
   const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
 
   const { searchParams } = new URL(req.url);
-  const code = searchParams.get('code');
+  const code = searchParams.get("code");
 
   if (code) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(new URL('/dashboard', req.url));
+  return NextResponse.redirect(
+    new URL("/dashboard", "https://todo-nine-rouge.vercel.app")
+  );
 }
